c2d824f2c6cf2d45a0c01c81bee0c06eda8ae6c7
PanaW <pana.wang@outlook.com>
2023-08-14 15:33:45 +0800

    add erc721

---
+++ scripts/deploy721.ts
@@ -0,0 +1,24 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const IceNftImpl = await ethers.deployContract("ICENFT721");
+    await IceNftImpl.waitForDeployment();
+
+    // console.log(
+    //     `IceImpl deployed to ${IceNftImpl.target}`
+    // );
+
+    const IceNft = await ethers.deployContract("ERC1967Proxy", [IceNftImpl.target, '0x8129fc1c']);
+    await IceNft.waitForDeployment();
+
+    console.log(
+        `IceNft 721 deployed to ${IceNft.target}`
+    );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
